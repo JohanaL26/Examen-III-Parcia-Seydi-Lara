c130de78b023abf3cfefe92190ace5a958aaaa2d
JohanaL26 <johanalara2606@gmail.com>
2022-04-25 20:43:14 -0600

    EXAMEN 3 SEYDI LARA

---
+++ BlazorJLExamenIII_SeydiLara/Modelos/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos;
+
+
+    public class Usuario
+    {
+        [Required(ErrorMessage = "El campo Código es obligatroio")]
+            public string Codigo { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatroio")]
+            public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Rol es obligatroio")]
+            public string Rol { get; set; }
+        //[Required(ErrorMessage = "El campo Codigo es obligatroio")]
+            public string Clave { get; set; }
+            public bool EstaActivo { get; set; }
+
+}
